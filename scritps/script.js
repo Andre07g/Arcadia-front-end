487147562aecd7ee3d90ce091316d0c193736f88
Karolain Reyes <reyeskarol0412@gmail.com>
2025-10-16 22:42:44 -0500

    refactor: :recycle: improve-css-js  enhance CSS and JavaScript for better styling and interactivity
     Refine CSS styles and JavaScript functionality to improve the overall
     appearance, responsiveness, and user interaction of the pages. This
     update optimizes the code for smoother animations and consistent design.
    
    by@KarolainReyes

--- scritps/script.js
+++ scritps/script.js
@@ -11,19 +11,35 @@ startBtn.addEventListener('click', () => {
   startBtn.disabled = true;
 
   const interval = setInterval(() => {
-    load += 2;
+    load += 5;
     progress.style.width = `${load}%`;
     percentage.textContent = `${load}%`;
 
     if (load >= 100) {
       clearInterval(interval);
       setTimeout(() => {
+        // Pantalla final con animación retro
         document.body.innerHTML = `
-          <div class="container">
-            <h1 class="title">WELCOME TO ARCADIA</h1>
-            <p style="font-size:10px;">Press any key to continue...</p>
+          <div class="container fade-in">
+            <h1 class="title neon-text">BIENVENIDO A ARCADIA</h1>
+            <p class="continue-text">Presione cualquier tecla para continuar...</p>
+              <div class="stars"></div>
+              <div class="stars2"></div>
+              <div class="stars3"></div>
           </div>
         `;
+
+        // Añadimos el listener de teclado (una sola vez)
+        document.addEventListener(
+          'keydown',
+          () => {
+            document.querySelector('.container').classList.add('fade-out');
+            setTimeout(() => {
+              window.location.href = '../pages/select.html';
+            }, 800);
+          },
+          { once: true }
+        );
       }, 500);
     }
   }, 150);
